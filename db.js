b32341671fc6a3158d457a2bf680a436a6176467
Yixing Shao <shao.yix@northeastern.neu>
2020-07-20 21:46:07 -0600

    change for mongodb

--- db.js
+++ db.js
@@ -1,22 +1,24 @@
-require('dotenv').config();
-const { MongoClient } = require('mongodb');
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 
 let db;
 
 async function connectToDb() {
-  const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
+  const url = process.env.MONGODB_URI || "mongodb://localhost/issuetracker";
   const client = new MongoClient(url, { useNewUrlParser: true });
   await client.connect();
-  console.log('Connected to MongoDB at', url);
+  console.log("Connected to MongoDB at", url);
   db = client.db();
 }
 
 async function getNextSequence(name) {
-  const result = await db.collection('counters').findOneAndUpdate(
-    { _id: name },
-    { $inc: { current: 1 } },
-    { returnOriginal: false },
-  );
+  const result = await db
+    .collection("counters")
+    .findOneAndUpdate(
+      { _id: name },
+      { $inc: { current: 1 } },
+      { returnOriginal: false }
+    );
   return result.value.current;
 }
 
